3a53cc2b18b171df5b1911fc7deb09269d0ef6bc
agaladima <arum.galadima@gmail.com>
2017-11-27 16:46:42 -0500

    Almost done
    1. Refresh the route when a tweet is posted 2. Include personal background image as page header background

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const moment = require('moment');
+//const moment = require('moment');
 const Twit = require('twit');
-const config = require('./config.js');
+const config = require('./js/config.js');
 const T = new Twit(config);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.locals.moment = require('moment');
 
 //use pug
 app.set('view engine', 'pug');
@@ -14,53 +16,67 @@ app.set('view engine', 'pug');
 // to use css
 app.use('/static', express.static('css'));
 
-// include main page route
-// const mainRoutes = require('./routes');
-// app.use(mainRoutes);
+app.post('/', (req, res)=>{
+	let tweetText = req.body.tweetSent;
+	T.post("statuses/update", { status: tweetText }).then(()=>{
+		res.redirect('/');
+	});
+});
 
 let myTwitter = [];
 let numFollowers = [];
+let profileImage = [];
 //get credentials
 T.get('account/verify_credentials', {skip_status: false}, function (err, data, response) {
 	//console.log(data);
-	myTwitter = '@' + data['screen_name'];
+	myTwitter = data['screen_name'];
 	numFollowers = data['followers_count'];
+	profileImage = data['profile_image_url'];
 });
 
 //get five most recent friends
-let friends = (req, res, next) => {
-		T.get('friends/list', { count: 5 },  function (err, data, response) {
-	  	friends = data;
-	    console.log(friends.users[0]);
-	});
-};
+let friends = {};
+T.get('friends/list', { count: 5 },  function (err, data, response) {
+	friends = data.users;
+	//console.log(friends[0]);
+});
 
 //get five most recent timeline tweets
+let timeline = {};
 T.get('statuses/user_timeline', {screen_name: "GSMbyAIR", count: 5}, function(err, data, response) {
 	//console.log("id ", req.screenName2);
-	//console.log("req.tweets ",data);
+	timeline = data;
+	//console.log(timeline);
 });
 
 //get five most recent DMs
+let directmssg = {};
 T.get('direct_messages', { count: 5 },  function (err, data, response) {
+	directmssg = data;
 	//console.log('direct_messages  ', data);
 });
 
-let tweet = (req, res, next) => {
-    T.post();
-};
-
+//render the data
 app.get('/', (req, res) => {
 	res.render('index', {
 		myTwit: myTwitter,
 		myFollowers: numFollowers,
+		profileImg: profileImage,
+		'elFriends': friends,
+		'Tweets': timeline,
+		'elDMs': directmssg
 	});
 });
 
-app.use(friends);
+//error pages
+app.use(function(req, res) {
+	res.status(400);
+	res.render('404.pug', {title: '404: File Not Found'});
+});
 
-app.get('/', function (req, res) {
-	req.params.friends;
+app.use(function(error, req, res, next) {
+	res.status(500);
+	res.render('500.pug', {title:'500: Internal Server Error', error: error});
 });
 
 app.listen(3000, () => {
